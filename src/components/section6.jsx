f8186d7d4a76bee999ed9fef3ee69c44241d9648
newbyy <alwin.puche16@gmail.com>
2023-05-08 23:01:44 +0800

    add lightbox

---
+++ src/components/section6.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function section6() {
+    return (
+        <section className='w-screen flex flex-row'>
+            <div className='w-screen min-h-[25px] bg-[#F5D061]' />
+            <h1>asdasdasd</h1>
+        </section>
+    )
+}
