04278ee6d92b09d26fae1801871b8540e77a1da4
newbyy <alwin.puche16@gmail.com>
2023-05-11 00:45:43 +0800

    add rating modal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,120 +11,166 @@ import { BsEmojiDizzyFill, BsEmojiFrownFill, BsEmojiExpressionlessFill, BsEmojiS
 export default function Home() {    
   const [render, setRender] = useState(0); 
   const [isOpen, setIsOpen] = useState(false);
+  const [rateModal, setRateModal] = useState(false);
+  const [rate, setRate] = useState(0);
   
   function closeModal() {
     setIsOpen(false);
+    setTimeout(() => {
+      setRateModal(true)
+    }, 3000);
+  }
+
+  function handleRate(rate) {
+    setRate(rate);
   }
 
   function handelModal(){
     setIsOpen(true);
   }
 
-  return ( 
-    <> 
-      {/* MODAL */}
-
-      <div  
-        className='h-screen overflow-hidden    w-screen scroll-smooth relative ' 
-        id="container" 
-        onClick={() => {
-          setRender(render => render + 1)
-        }} 
-      >
-        <div className="h-full w-full relative overflow-y-scroll overflow-x-hidden">
+  return (  
+    <div  
+      className='h-screen overflow-hidden    w-screen scroll-smooth relative ' 
+      id="container" 
+      onClick={() => {
+        setRender(render => render + 1)
+      }} 
+    >
+      <div className="h-full w-full relative overflow-y-scroll overflow-x-hidden">
+      
+        <Head>
+          <link rel="icon" href="/logo.svg"   />
+          <title className="sacramento">Light and Focus</title>
+        </Head>
+        {/* BACKGROUND MUSIC */}
+        <audio  id='bgMusic' controls className="invisible absolute" loop  src="/sfx/bg.mp3"  />  
         
-          <Head>
-            <link rel="icon" href="/logo.svg"   />
-            <title className="sacramento">Light and Focus</title>
-          </Head>
-          {/* BACKGROUND MUSIC */}
-          <audio  id='bgMusic' controls className="invisible absolute" loop  src="/sfx/bg.mp3"  />  
-          
-          {/* HEADER */}
-          <Header />   
+        {/* HEADER */}
+        <Header />   
 
-          {/* SECTION 1 */}
-          <div>
-            <section className="h-auto md:py-32 w-screen flex items-center justify-evenly flex-col md:flex-row" >
-              <div className="relative w-10/12 md:min-w-[300px] md:max-w-[400px]  md:max-h-[400px] sm:w-10/12 md:w-3/12 overflow-hidden rounded-3xl hover:shadow-md h-96 m-5 transition-all duration-500">
-                <Image  
-                  className="image1"
-                  color="black" 
-                  alt="logo"
-                  fill
-                  src='/image1.png'
-                /> 
-              </div>
-              <div   className="flex flex-col flex-wrap md:w-5/12 w-10/12 items-center" > 
-                <motion.div 
-                  viewport={{once: false}}
-                  onViewportEnter={() => render ? document.getElementById('bgMusic').pause() : null} 
-                  onViewportLeave={() => render ? document.getElementById('bgMusic').play() : null}
-                >
-                <h1 className="sacramento text-4xl text-center" >Welcome to Light and Focus</h1>
-                </motion.div>
-                <p className="text-clip overflow-hidden text-justify leading-7 tracking-wider">
-                  Is in focus if light from object points has been converged nearly as much as possible in the image, and out of focus if light has not been well converged. The border between these is sometimes defined by a “circle of confusion “ criterion which allows us to convey information and more importantly, emotion in an image.
-                </p>
-              </div>
-            </section>
-          </div>
-
-          <div>
-            {/* SECTION 2 */}
-            <Section2 />
-
-
-            {/* SECTION 3 */}
-            <Section3 />
-          </div>
-
-          {/* SECTION 4 */} 
-          <Section4  />
-
-          {/* SECTION 5 */} 
-          <Section5/>  
-          
-          
-          <ReactModal   
-            portalClassName='md:w-[50%] w-[80%]'
-            isOpen={isOpen}
-            onRequestClose={closeModal}
-            style={{
-              overlay: {
-                backgroundColor: 'rgba(0, 0, 0, 0.80)', 
-              },
-              content: {    
-                top: '50%',
-                left: '50%',
-                right: 'auto', 
-                bottom: 'auto', 
-                marginRight: '-50%',
-                transform: 'translate(-50%, -50%)',
-              }
-            }}
-          >
-            <div className="md:p-10 space-y-5">
-              <h1 className="sacramento text-xl font-bold tracking-wider md:text-5xl text-center ">Thank you for messaging us!</h1>
-              <hr />
-              <h1 className="text-xl roboto ">Rate us: </h1>
-              <div className="flex space-x-5 justify-evenly mt-5">
-                <BsEmojiDizzy color="gray"  className="hover:fill-[red] hover:cursor-pointer hover:scale-125 transition-all text-2xl md:text-3xl" />
-                <BsEmojiFrown color="gray"  className="hover:fill-[orange] hover:cursor-pointer hover:scale-125 transition-all text-2xl md:text-3xl"  />
-                <BsEmojiExpressionless color="gray"  className="hover:fill-[yellow] hover:cursor-pointer hover:scale-125 transition-all text-2xl md:text-3xl"  />
-                <BsEmojiSmile color="gray"  className="hover:fill-[yellowgreen] hover:cursor-pointer hover:scale-125 transition-all text-2xl md:text-3xl"  />
-                <BsEmojiLaughing color="gray"  className="hover:fill-[green] hover:cursor-pointer hover:scale-125 transition-all text-2xl md:text-3xl"  />
-              </div>
+        {/* SECTION 1 */}
+        <div>
+          <section className="h-auto md:py-32 w-screen flex items-center justify-evenly flex-col md:flex-row" >
+            <div className="relative w-10/12 md:min-w-[300px] md:max-w-[400px]  md:max-h-[400px] sm:w-10/12 md:w-3/12 overflow-hidden rounded-3xl hover:shadow-md h-96 m-5 transition-all duration-500">
+              <Image  
+                className="image1"
+                color="black" 
+                alt="logo"
+                fill
+                src='/image1.png'
+              /> 
             </div>
-          </ReactModal>
-
-          {/* SECTION 6 */} 
-          <Section6  render={render} handleClick={handelModal} />  
-          
-          <Footer render={render} />
+            <div   className="flex flex-col flex-wrap md:w-5/12 w-10/12 items-center" > 
+              <motion.div 
+                viewport={{once: false}}
+                onViewportEnter={() => render ? document.getElementById('bgMusic').pause() : null} 
+                onViewportLeave={() => render ? document.getElementById('bgMusic').play() : null}
+              >
+              <h1 className="sacramento text-4xl text-center" >Welcome to Light and Focus</h1>
+              </motion.div>
+              <p className="text-clip overflow-hidden text-justify leading-7 tracking-wider">
+                Is in focus if light from object points has been converged nearly as much as possible in the image, and out of focus if light has not been well converged. The border between these is sometimes defined by a “circle of confusion “ criterion which allows us to convey information and more importantly, emotion in an image.
+              </p>
+            </div>
+          </section>
         </div>
+
+        <div>
+          {/* SECTION 2 */}
+          <Section2 />
+
+
+          {/* SECTION 3 */}
+          <Section3 />
+        </div>
+
+        {/* SECTION 4 */} 
+        <Section4  />
+
+        {/* SECTION 5 */} 
+        <Section5/>  
+        
+        
+        <ReactModal   
+          portalClassName='md:w-[50%] w-[80%]'
+          isOpen={isOpen}
+          onRequestClose={closeModal}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.80)', 
+            },
+            content: {    
+              top: '50%',
+              left: '50%',
+              right: 'auto', 
+              bottom: 'auto', 
+              marginRight: '-50%',
+              transform: 'translate(-50%, -50%)',
+            }
+          }}
+        >
+          <div className="md:p-5 space-y-10">
+            <h1 className="sacramento text-xl font-bold tracking-wider md:text-5xl text-center ">Thank you for messaging us!</h1>
+             <button className="text-center w-full hover:bg-black hover:bg-opacity-10 py-2" onClick={closeModal}>Close</button>
+          </div>
+        </ReactModal>
+
+        <ReactModal   
+          portalClassName='md:w-[50%] w-[80%]'
+          isOpen={rateModal}
+          onRequestClose={() =>{ 
+            setRateModal(false);
+            setRate(0)
+          }}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.80)', 
+            },
+            content: {    
+              top: '50%',
+              left: '50%',
+              right: 'auto', 
+              width: '30%',
+              bottom: 'auto', 
+              marginRight: '-50%',
+              transform: 'translate(-50%, -50%)',
+            }
+          }}
+        >
+          <div className="md:p-5 space-y-10 w-full">
+            <h1 className="sacramento text-xl font-bold tracking-wider md:text-5xl text-center ">Rate us!</h1>
+            <div>
+              <input className="shadow-lg w-full px-3 py-2 text-xl text-center" placeholder="example@gmail.com" />
+            </div>
+            <div className="flex justify-evenly">
+              <BsEmojiDizzy className={`transition-all  ${rate == 1 ? 'text-[red] scale-125' : 'hover:cursor-pointer hover:scale-125 hover:text-[red] '} text-4xl  `} onClick={() => handleRate(1)} />
+              <BsEmojiFrown className={`transition-all  ${rate == 2 ? 'text-[orange] scale-125' : 'hover:text-[orange] hover:scale-125 hover:cursor-pointer'} text-4xl  `} onClick={() => handleRate(2)} />
+              <BsEmojiExpressionless className={`transition-all ${rate == 3 ? 'text-[#e8ce0d] scale-125' : 'hover:cursor-pointer hover:scale-125 hover:text-[#e8ce0d]'  }  text-4xl `} onClick={() => handleRate(3)} />
+              <BsEmojiSmile className={`transition-all ${rate == 4 ? 'scale-125 text-[yellowgreen]' : 'hover:cursor-pointer hover:scale-125 hover:text-[yellowgreen]'}   text-4xl `} onClick={() => handleRate(4)} />
+              <BsEmojiLaughing className={`transition-all ${rate == 5 ? 'text-[green] scale-125' : 'hover:cursor-pointer hover:scale-125 hover:text-[green]'}  text-4xl   `} onClick={() => handleRate(5)} />
+            </div>
+            <div className="flex flex-col w-full space-y-2">
+              <button className="text-center w-full hover:bg-[#F5D061] bg-[#F5D061] bg-opacity-20 hover:bg-opacity-80 py-2  font-medium" >Send rate</button>
+              <button 
+                className="hover:bg-blue-300 bg-blue-300 bg-opacity-20 py-2 hover:bg-opacity-80" 
+                onClick={() => {
+                  setRateModal(false)
+                  setRate(0);
+                }}
+              >
+                close
+              </button>
+            </div>
+          </div>
+        </ReactModal>
+        
+        {/* SECTION 6 */} 
+        <Section6  render={render} handleClick={handelModal} />  
+        
+        <Footer render={render} />
       </div>
-    </>
+    </div> 
   )
 }
 
